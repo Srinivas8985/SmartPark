3d35635161b948902e6e721d0a7b3b534c084990
Srinivas8985 <goud1632@gmail.com>
2026-04-15 22:41:33 +0530

    cors updated

--- server/server.js
+++ server/server.js
@@ -22,7 +22,11 @@ app.set('io', io);
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token']
+}));
 
 // Database Connection
 mongoose.connect(process.env.MONGO_URI)
